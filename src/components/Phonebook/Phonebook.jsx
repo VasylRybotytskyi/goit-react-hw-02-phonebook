b6edc5617b76407184d443e183a61d65b5881422
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-04-17 21:27:48 +0300

    added new function handleAddContact

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,14 +1,10 @@
 import { Contacts } from 'components/Contacts/Contacts';
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 export class Phonebook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     name: '',
   };
 
@@ -17,6 +13,12 @@ export class Phonebook extends Component {
   //     contacts: prevState.contacts.filter(contact => contact.id !== contactId),
   //   }));
   // };
+
+  handleAddContact = () => {
+    const { contacts, name } = this.state;
+    const newContact = { id: nanoid(), name };
+    this.setState({ contacts: [...contacts, newContact], name: '' });
+  };
 
   render() {
     const { contacts } = this.state;
@@ -31,8 +33,12 @@ export class Phonebook extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            value={this.state.name}
+            onChange={e => this.setState({ name: e.target.value })}
           />
-          <button type="button">Add contact</button>
+          <button type="button" onClick={this.handleAddContact}>
+            Add contact
+          </button>
         </>
         <Contacts contacts={contacts} onDeleteContact={this.deleteContact} />
       </>
